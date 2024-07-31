7b294e608fb740688215ae607a47b6721e7bb3f7
Hifive <2019912635@qq.com>
2024-07-31 10:46:40 +0800

    debug

--- serve/serve_predict.py
+++ serve/serve_predict.py
@@ -9,12 +9,9 @@ from keras.models import load_model
 model = load_model("test_kick_1.h5")
 
 cat_dict = [
-    "Jungle",
     "Top",
     "Chest",
     "Signature",
-    "Thump",
-    "Vinyl",
     "Stomp",
     "Punchy",
     "808s",
@@ -27,14 +24,15 @@ cat_dict = [
 
 # 定义 Flask 应用
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1000 * 1000
 
 
 # 定义一个函数来处理音频文件并返回预测结果
 def process_audio(file):
     # 加载音频文件
-    y, sr = librosa.load(file, sr=16000)
+    y, sr = librosa.load(file)
     # 提取特征
-    n_fft = min(2048, len(y))
+    n_fft = max(2048*3, min(2048, len(y)))
     mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, n_fft=n_fft).T
     # 预测
     prediction = model.predict(tf.ragged.constant([mfccs]))
@@ -47,23 +45,27 @@ def predict():
     # 检查是否有文件上传
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
-    file = request.files['file']
+    file = request.files["file"]
     # 检查文件是否为音频文件
     if file.filename == '':
         return jsonify({"error": "No selected file"}), 400
-    if file and allowed_file(file.filename):
+    if file and allowed_file(file):
         # 处理音频文件并返回预测结果
         prediction = process_audio(file)
         item = np.argmax(prediction)
         cat_name = cat_dict[item]
         return jsonify({"prediction": cat_name, "probabilities" : prediction.tolist()})
+    else:
+        return jsonify({"error": "File type is not allowed or too large"}), 400
 
 
 # 定义一个辅助函数来检查文件类型
-def allowed_file(filename):
+def allowed_file(file):
+    filename = file.filename
     ALLOWED_EXTENSIONS = {'wav', 'mp3', 'aif'}
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS \
+           and file.stream.tell() < 10
 
 
 # 定义一个路由来显示上传表单
